4445ac85d17593aa663b1ac870d03fee717e2a86
aguspray001 <aguspray001@gmail.com>
2021-07-17 20:58:21 +0700

    init

--- pages/login/index.js
+++ pages/login/index.js
@@ -14,6 +14,7 @@ export default function Login() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const router = useRouter();
+  // oke
 
   const onSubmit = () =>{
     if(email !== '' && password !== ''){
